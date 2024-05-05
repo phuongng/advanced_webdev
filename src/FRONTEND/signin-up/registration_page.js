09938f63b21449edbe2f38d39d54e8b5f4836e23
ByronD21 <doyal.byron@yahoo.com>
2024-05-05 18:29:20 -0400

    Hooked up login to use endpoint, edited registration endpoint to hashpassword, removed unused imports

--- src/FRONTEND/signin-up/registration_page.js
+++ src/FRONTEND/signin-up/registration_page.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import "./signin-up_css/registration.css";
 import api from '../../api.js';
-import axios from 'axios';
 import { FaUpload } from "react-icons/fa6";
 
 function Registration() {
